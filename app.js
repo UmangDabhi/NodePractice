3f1779e81c1c42e861c9be1e91c177d9ec082f85
Umang Dabhi <ud30104@gmail.com>
2023-12-30 23:29:20 +0530

    Connected with mongodb and did the demo practice

--- app.js
+++ app.js
@@ -1,27 +1,59 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
-//listen to request
-app.listen(3000);
+const dbURI = 'mongodb+srv://umang:Umang%401234@cluster0.yx7xkkd.mongodb.net/node-tuts?retryWrites=true&w=majority';
+mongoose.connect(dbURI)
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
+
+
+
+app.use(express.static('public'));
+
+
+app.get('/add-blog',(req,res)=>{
+    const blog = new Blog({
+        title: 'new blog 2',
+        snippet: 'about new blog',
+        body: 'more about my new blog'
+    });
+    blog.save()
+    .then((result)=>{
+        res.send(result)
+    }).catch((err)=> console.log(err));
+});
+
+app.get('/all-blogs',(req,res)=>{
+    Blog.find().then((result)=>{
+        res.send(result);
+    }).catch((err)=>console.log(err));
+});
+app.get('/single-blog',(req,res)=>{
+    Blog.findById('659058d265a83780e2e77250').then((result)=>{
+        res.send(result);
+    }).catch((err)=>console.log(err));
+});
 
 const blogs = [
-    {title: "First", snippet: "First blog"},
-    {title: "Second", snippet: "Second blog"},
-    {title: "Third", snippet: "Third blog"},
+    { title: "First", snippet: "First blog" },
+    { title: "Second", snippet: "Second blog" },
+    { title: "Third", snippet: "Third blog" },
 ];
 
 app.get('/', (req, res) => {
-    res.render('index', { title: "Home" ,blogs });
+    res.render('index', { title: "Home", blogs });
 });
 app.get('/about', (req, res) => {
-    res.render('about',{ title: "about"});
+    res.render('about', { title: "about" });
 });
-app.get('/blogs/create',(req,res)=>{
-    res.render('create',{ title: "Create a new Blog"});
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: "Create a new Blog" });
 })
 
 app.use((req, res) => {
-    res.status(404).render('404',{ title: "404"});
+    res.status(404).render('404', { title: "404" });
 })
